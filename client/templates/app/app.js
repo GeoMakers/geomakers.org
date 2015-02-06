15feeef18883dc7eea347cf576a8580068d95027
Simon Engelhardt <simon@engelhardt.io>
2015-02-06 17:21:21 -0500

    Replace quick-and-dirty gravatar implementation with avatar package
    Also supports avatars retrieved through OAuth (Twitter, Facebook, etc.)

---
+++ client/templates/app/app.js
@@ -0,0 +1,5 @@
+Meteor.startup(function() {
+  Avatar.options = {
+    gravatarDefault: "mm"
+  };
+});
--- client/templates/users/users.js
+++
@@ -1,7 +0,0 @@
-Template.profilePic.helpers({
-  gravatarImageUrl: function() {
-    return Gravatar.imageUrl(this.emails[0].address, {
-      default: 'mm'
-    });
-  }
-});
