e4e888b852cfcc21609497d755af77df1c3b8e5d
Simon Engelhardt <simon@engelhardt.io>
2015-02-12 10:37:36 -0500

    Get profile body from document

--- server/publications.js
+++ server/publications.js
@@ -1,4 +1,4 @@
 // Publish certain fields publicly for all users (needed for linking to user profiles etc.)
 Meteor.publish('publicUsers', function() {
-  return Meteor.users.find({}, {fields: {'profile.name': 1}});
+  return Meteor.users.find({}, {fields: {'profile.name': 1, 'profile.body': 1}});
 });
