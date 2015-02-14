df8b8eb1b8b86a42e7d561682c4120dd09f7ff1d
Simon Engelhardt <simon@engelhardt.io>
2015-02-13 20:06:24 -0500

    Add gallery
    Still needs lightbox.
    
    WIP #5

---
+++ client/templates/gallery/gallery.js
@@ -0,0 +1,16 @@
+Template.gallery.rendered = function() {
+  var $gallery = this.$(this.firstNode);
+
+  // Set up observation, so gallery will be automatically reinitialized if images change
+  this.autorun(function() {
+    // Establish dependency on gallery images
+    Template.currentData();
+
+    // Schedule gallery initialization after DOM has been updated
+    Tracker.afterFlush(function() {
+      $gallery.justifiedGallery({
+        captions: false
+      });
+    });
+  });
+};
