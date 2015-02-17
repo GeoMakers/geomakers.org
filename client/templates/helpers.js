d9358ebc7ed496c902ce16763445307a9f1c9ced
Simon Engelhardt <simon@engelhardt.io>
2015-02-17 09:47:04 -0500

    Fix preview of bodies with no content

--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -1,4 +1,5 @@
 Template.registerHelper('preview', function(value, options) {
+  if (!value) return;
   var length = options.hash.length || 140;
   var content = value.replace(/<(?:.|\n)*?>/gm, ' ');
   if(content.length > length) content = content.substring(0, length-1).trim() + '…';
