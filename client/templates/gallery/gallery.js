f435dae5255f87120d3fe502243151b0cf466ea3
Simon Engelhardt <simon@engelhardt.io>
2015-02-18 00:45:12 -0500

    Add sliding gallery to dream page
    Had to use an unpackaged version of the slick library (hence the files in
    client/lib/slick and public/lib/slick) because of bugs in the available
    [packaged version](https://atmospherejs.com/udondan/slick)
    that I am not currently able to fix with a simlple PR because of what
    seems to be an issue in a tool used by the Meteor build tool. This issue
    [has been fixed](https://github.com/reworkcss/css/commit/46d2b22aa43697ae78bd829f2770895b7da6883e#diff-af082abe86db1a3b14c5be4a766b48b1R464),
    but it does not work in Meteor build as of version 1.0.3.1
    
    WIP #5 - needs to use resized thumbnails when the image-resizing branch has been merged in

--- client/templates/gallery/gallery.js
+++ client/templates/gallery/gallery.js
@@ -1,6 +1,9 @@
 Template.gallery.helpers({
   index: function() {
     return Template.parentData().indexOf(this) + 1;
+  },
+  hasMultipleImages: function() {
+    return this.length > 1;
   }
 });
 
@@ -12,13 +15,28 @@ Template.gallery.rendered = function() {
     // Establish dependency on gallery images
     var images = Template.currentData();
 
-    // Schedule gallery initialization after DOM has been updated
+    // Un-slick sliders if they have already been slicked
+    if ($gallery.find('.selected-image')[0] && $gallery.find('.selected-image')[0].slick) $gallery.find('.selected-image').slick('unslick');
+    if ($gallery.find('.thumbnails')[0] && $gallery.find('.thumbnails')[0].slick) $gallery.find('.thumbnails').slick('unslick');
+
+    // Schedule slider initialization after DOM has been updated
     Tracker.afterFlush(function() {
-      if (images.length > 1) {
-        $gallery.justifiedGallery({
-          captions: false
-        });
-      }
+      $gallery.find('.selected-image').slick({
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       arrows: false,
+       adaptiveHeight: true,
+       asNavFor: '.thumbnails'
+      });
+      $gallery.find('.thumbnails').slick({
+       slidesToShow: 5,
+       slidesToScroll: 1,
+       arrows: false,
+       centerMode: true,
+       focusOnSelect: true,
+       dots: true,
+       asNavFor: '.selected-image'
+      });
     });
   });
 };
