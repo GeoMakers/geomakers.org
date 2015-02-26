14813915892e97e34bc5d2c51358a726d3912036
Simon Engelhardt <simon@engelhardt.io>
2015-02-26 16:07:32 -0500

    Experimental drawing of logo in jumbotron
    Maybe a bit too much.
    
    Not finished (does not work when navigating back to the front page).

--- client/templates/app/app.js
+++ client/templates/app/app.js
@@ -9,3 +9,14 @@ Template.appBody.helpers({
     return Router.current().route.getName();
   }
 })
+
+Template.appBody.rendered = function() {
+  SVGInjector(this.$('img[src$=".svg"]'), {
+    each: function(svg) {
+      svg.setAttribute('class', 'drawing');
+      new Vivus(svg, {type: 'delayed', duration: 150}, function() {
+        this.el.removeAttribute('class');
+      });
+    }
+  });
+};
