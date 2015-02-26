a195c5b669b3f9afe675b5a16156721144dc62d6
Simon Engelhardt <simon@engelhardt.io>
2015-02-26 12:42:43 -0500

    Add drawing effect to logo in footer
    Solely for the sake of eye candy

--- client/templates/app/app.js
+++ client/templates/app/app.js
@@ -11,5 +11,12 @@ Template.appBody.helpers({
 })
 
 Template.appBody.rendered = function() {
-  SVGInjector(this.$('img[src$=".svg"]'));
+  SVGInjector(this.$('img[src$=".svg"]'), {
+    each: function(svg) {
+      svg.setAttribute('class', 'drawing');
+      new Vivus(svg, {type: 'delayed', duration: 150}, function() {
+        this.el.setAttribute('class', 'drawn');
+      });
+    }
+  });
 };
