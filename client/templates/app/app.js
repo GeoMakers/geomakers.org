64cf5196f1d6c6764efffb80527e4ed9d476922d
Simon Engelhardt <simon@engelhardt.io>
2015-03-02 13:25:23 -0500

    Affix aside content

--- client/templates/app/app.js
+++ client/templates/app/app.js
@@ -9,3 +9,22 @@ Template.appBody.helpers({
     return Router.current().route.getName();
   }
 })
+
+Template.aside.rendered = function() {
+  var $affix = this.$('.aside-content');
+
+  // Wait for ad image to be loaded
+  this.$('.ad img').load(function() {
+    // Affix aside content
+    $affix.affix({
+      offset: {
+        top: function() {
+          return (this.top = $affix.offset().top - 20); // 20 should change if @line-height-computed changes
+        },
+        bottom: function () {
+          return (this.bottom = $('footer').outerHeight(true));
+        }
+      }
+    });
+  });
+}
