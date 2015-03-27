c9cf144b6308ed5497698f61f1fe9471a2e42158
Simon Engelhardt <simon@engelhardt.io>
2015-03-27 00:45:06 -0400

    Add reactivity to masonry layout of recipes board

--- client/templates/recipes/recipes.js
+++ client/templates/recipes/recipes.js
@@ -1,8 +1,19 @@
-Template.recipesBoard.rendered = function() {
-  var masonry = new Masonry(this.firstNode, {
-    itemSelector: '.recipe'
+Template.recipesBoard.onRendered(function() {
+  var boardNode = this.firstNode;
+
+  this.autorun(function() {
+    // Set up a dependency on all content of the board (so a new masonry layout is created both when items are added or removed as well as changed)
+    Template.currentData().recipes.fetch();
+
+    // Schedule creation of a new board layout after flush (DOM updated)
+    Tracker.afterFlush(function() {
+      // Wait for images to load
+      imagesLoaded(boardNode, function() {
+        // Create new masonry (even if one exists, calling layout is not enough as new items may have been added or removed)
+        var masonry = new Masonry(boardNode, {
+          itemSelector: '.recipe'
+        });
+      });
+    });
   });
-  imagesLoaded(this.firstNode, function() {
-    masonry.layout();
-  });
-};
+});
