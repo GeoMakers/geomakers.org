456eadc6feb2e318b1be41dfd771c4fd23b28eb0
Simon Engelhardt <simon@engelhardt.io>
2015-03-28 11:05:30 -0400

    Create initial masonry when board has rendered
    So the masonry layout looks better while the user is waiting for the images
    to load

--- client/templates/recipes/recipes.js
+++ client/templates/recipes/recipes.js
@@ -8,6 +8,9 @@ Template.recipe.helpers({
 
 Template.recipesBoard.onRendered(function() {
   var boardNode = this.firstNode;
+  var masonry = new Masonry(boardNode, {
+    itemSelector: '.recipe'
+  });
 
   this.autorun(function() {
     // Set up a dependency on all content of the board (so a new masonry layout is created both when items are added or removed as well as changed)
@@ -17,10 +20,7 @@ Template.recipesBoard.onRendered(function() {
     Tracker.afterFlush(function() {
       // Wait for images to load
       imagesLoaded(boardNode, function() {
-        // Create new masonry (even if one exists, calling layout is not enough as new items may have been added or removed)
-        var masonry = new Masonry(boardNode, {
-          itemSelector: '.recipe'
-        });
+        masonry = new Masonry(boardNode, masonry.options);
       });
     });
   });
