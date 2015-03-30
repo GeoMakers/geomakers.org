f9d1f5fe5ad564cf6450ea9c477b6bad9910d515
Simon Engelhardt <simon@engelhardt.io>
2015-03-30 18:34:33 -0400

    Use feed for activities page
    Through refactoring of the dreams feed

--- client/templates/dreams/dreams.js
+++ client/templates/dreams/dreams.js
@@ -1,13 +1,10 @@
-var getQuery = function() {
-  return {
-    dream: this._id
+Template.dreamActions.helpers({
+  getQuery: function() {
+    return {
+      dream: this._id
+    };
+  },
+  class: function() {
+    return Router.current().route.getName() === 'dream' ? 'btn btn-make' : '';
   }
-};
-
-Template.dreamsList.helpers({
-  getQuery: getQuery
-});
-
-Template.dream.helpers({
-  getQuery: getQuery
 });
