a24b99240e181ac21b3561c1d05c4068106d0307
Simon Engelhardt <simon@engelhardt.io>
2015-04-22 19:18:02 -0400

    Remove non-breaking spaces from body previews
    The WYSIWYG seems to insert these if a paragraph starts with a space.
    
    Closes #23

--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -1,7 +1,7 @@
 Template.registerHelper('preview', function(value, options) {
   if (!value) return;
   var length = options.hash.length || 140;
-  var content = value.replace(/<(?:.|\n)*?>/gm, ' ');
+  var content = value.replace(/<(?:.|\n)*?>|&nbsp;/gm, ' ');
   if(content.length > length) content = content.substring(0, length-1).trim() + '…';
   return content;
 });
