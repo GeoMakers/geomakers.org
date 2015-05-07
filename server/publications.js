26aaff5658fd384ec9a17782618ef6d93534828a
Simon Engelhardt <simon@engelhardt.io>
2015-05-06 20:00:22 -0400

    Add fields required for avatars to public users publication

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,14 @@
 // Publish certain fields publicly for all users (needed for linking to user profiles etc.)
 Meteor.publish('publicUsers', function() {
-  return Meteor.users.find({}, {fields: {'profile.name': 1, 'profile.body': 1}});
+  return Meteor.users.find({}, {
+    fields: {
+      'profile.name': 1,
+      'profile.body': 1,
+      'services.facebook.id': 1,
+      'services.twitter.profile_image_url_https': 1,
+      'services.google.picture': 1
+    }
+  });
 });
 
 Meteor.publish('images', function() {
